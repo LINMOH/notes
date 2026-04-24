9595802a51377d70a241368f51a2231e7445e8de
Lin Mohan <linmhwork@outlook.com>
2026-04-25 02:41:10 +0800

    二分

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,23 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
 export default defineNuxtConfig({
-  extends: ['docus']
+  extends: ['docus'],
+
+  css: ['katex/dist/katex.min.css'],
+
+  content: {
+    markdown: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex]
+    },
+    build: {
+      markdown: {
+        highlight: {
+          langs: ['cpp', 'c', 'python', 'rust']
+        }
+      }
+    }
+  }
 })
